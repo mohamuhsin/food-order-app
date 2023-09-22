d1eda6bc86b6cdf0a999f856a496e477fe06afc5
mohamuhsin <muhsinohoho@outlook.com>
2023-09-22 19:25:05 +0300

    working on the meals

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import classes from "./AvailableMeals.module.css";
+
 const DUMMY_MEALS = [
   {
     id: "m1",
@@ -27,7 +29,12 @@ const DUMMY_MEALS = [
   },
 ];
 const AvailableMeals = () => {
-  return;
+  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  return (
+    <section className={classes.meals}>
+      <ul>{mealsList}</ul>
+    </section>
+  );
 };
 
 export default AvailableMeals;
