8af25dcca40448779f18289dc50eb129b538d185
mohamuhsin <muhsinohoho@outlook.com>
2023-09-22 18:30:17 +0300

    working on the header cart utton

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,8 +1,12 @@
 import React from "react";
 
 const HeaderCartButton = (props) => {
-    return(
-
-    )
+  return (
+    <button>
+      <span></span>
+      <span></span>
+      <span></span>
+    </button>
+  );
 };
 export default HeaderCartButton;
