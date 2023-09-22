e2736afd6b100b183fc5f3288eeaf79fefd21ce3
mohamuhsin <muhsinohoho@outlook.com>
2023-09-22 18:33:59 +0300

    done with header button

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,11 +1,16 @@
 import React from "react";
 
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
+
 const HeaderCartButton = (props) => {
   return (
-    <button>
-      <span></span>
-      <span></span>
-      <span></span>
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span className={classes.badge}>Your Cart</span>
+      <span>3</span>
     </button>
   );
 };
