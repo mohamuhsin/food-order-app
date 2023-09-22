75dc62918d4dd2d208262be38734ff7088631c5e
mohamuhsin <muhsinohoho@outlook.com>
2023-09-22 19:27:24 +0300

    done with meals

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,5 +1,15 @@
-import React from "react";
+import React, { Fragment } from "react";
 
-const Meals = () => {};
+import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
+
+const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummary />
+      <AvailableMeals />
+    </Fragment>
+  );
+};
 
 export default Meals;
