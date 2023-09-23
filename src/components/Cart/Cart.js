c772730f5907339abf4cf0ba09a233608b4cf80a
mohamuhsin <muhsinohoho@outlook.com>
2023-09-23 20:30:22 +0300

    working on cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,16 @@
 import classes from "./Cart.module.css";
 
 const Cart = () => {
-  return;
+  const cartItem = [
+    { id: "c1", name: "sushi", amount: "2", price: "12.99" },
+  ].map((item) => <li>{item.name}</li>);
+  return (
+    <div>
+      cartitems
+      <div></div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Cart;
